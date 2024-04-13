bf8fe4cf5829e4c135404dbec7334aa5ea6afafd
Nont18 <112168761+Nont18@users.noreply.github.com>
2024-04-14 00:24:37 +0700

    initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_LQY963DLa', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1jk5e2a2guhcr91j883fb7g2b1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://e9fdz0ngmg.execute-api.us-west-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
